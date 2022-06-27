b47b11697e9ab6ac57f93eab92ab9308e27186d4
Omachonu Prosper <omachonucodes@gmail.com>
2022-06-27 10:16:47 +0100

    Indentent code using 4 spaces(tabs)

--- index.php
+++ index.php
@@ -1,30 +1,30 @@
 <?php 
 
-  // Mock up Data  
-  $tasks = [
-    [
-      'id' => '1',
-      'title' => 'Wash the dishes',
-      'status' => 'finished',
-      'create_date' => '27-06-2022',
-      'authors_id' => 1
-    ],
-    [
-      'id' => '2',
-      'title' => 'Run laps',
-      'status' => 'unfinished',
-      'create_date' => '27-06-2022',
-      'authors_id' => 1
-    ]
-  ];
+    // Mock up Data  
+    $tasks = [
+        [
+            'id' => '1',
+            'title' => 'Wash the dishes',
+            'status' => 'finished',
+            'create_date' => '27-06-2022',
+            'authors_id' => 1
+        ],
+        [
+            'id' => '2',
+            'title' => 'Run laps',
+            'status' => 'unfinished',
+            'create_date' => '27-06-2022',
+            'authors_id' => 1
+        ]
+    ];
 
-  $user = [
-    'id' => '1',
-    'username' => 'John Doe',
-    'email' => 'JohnDoe@todoapp.com',
-    'password' => 'johndoe123',
-    'join_date': '01-06-2022'
-  ];
+    $user = [
+        'id' => '1',
+        'username' => 'John Doe',
+        'email' => 'JohnDoe@todoapp.com',
+        'password' => 'johndoe123',
+        'join_date' => '01-06-2022'
+    ];
 
 ?>
 
--- templates/header.php
+++ templates/header.php
@@ -1,17 +1,18 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- Font awesome CDN -->
-    <link rel="stylesheet" type="text/css" href="./assets/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <!-- Font awesome CDN -->
+        <link rel="stylesheet" type="text/css" href="./assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="./assets/css/styles.css">
 
 
-    <title>Todo list app by Omachonu Prosper</title>
-  </head>
-  <body>
+      <title>Todo list app by Omachonu Prosper</title>
+    </head>
+    <body>
+

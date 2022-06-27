cb8dc4a8b4fead9111233fc6c1ca2f5f04986205
Omachonu Prosper <omachonucodes@gmail.com>
2022-06-28 00:27:33 +0100

    Redesigned index page

--- index.php
+++ index.php
@@ -29,7 +29,62 @@
 ?>
 
 <?php require_once './templates/header.php' ?>
+<?php require_once './templates/navbar.php' ?>
 
-   
+    <main>
+        <div class="container mx-auto mt-5 pl-3 pr-3">
+            <div class="form-box card mb-5">
+                <div class="card-body">
+                    <form method="post" action="">
+                        <div class="form-row">
+                            <div class="form-group mb-md-0 col-md-auto task-input">
+                                <label class="sr-only">Task title</label>
+                                <input type="text" class="form-control" id="task-title" name="title" placeholder="Task title">
+                            </div>
+
+                            <div class="col-md-auto submit-task">
+                                <button type="submit" name="submit" class="btn primary-background">Add Task</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="cards">
+                <!-- Display All tasks -->
+                <?php foreach($tasks as $task) { ?>
+                    <div class="card task mb-4" data-id="<?php echo $task['id'] ?>">
+                        <div class="card-body">
+                            <div>
+                                <h5 class="card-title"><?php echo $task['title'] ?></h5>
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    Created <?php echo $task['create_date'] ?>
+                                </h6>
+                            </div>
+                            
+                            <div class="row text-center pt-3">
+                                <div class="col">
+                                    <a href="#" class="card-link task-icon">
+                                        <i class="fa fa-trash trash"></i>
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a href="#" class="card-link task-icon">
+                                        <i class="fa fa-pencil pencil"></i>
+                                    </a>
+                                </div>
+                                <div class="col">
+                                    <a href="#" class="card-link task-icon">
+                                        <i class="fa fa-check-square-o check"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
+
 
 <?php require_once './templates/footer.php' ?>
---
+++ templates/navbar.php
@@ -0,0 +1,18 @@
+<nav class="navbar navbar-dark navbar-expand-sm">
+    <a class="navbar-brand" href="./">Todo-app</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#"><?php echo $user['username']; ?></a>
+            </li>
+        </ul>
+    </div>
+</nav>

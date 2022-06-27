6c812af5b334983e37cd6f9a0e22fae426dd3fda
Omachonu Prosper <omachonucodes@gmail.com>
2022-06-27 10:09:18 +0100

    Create Mock up data and drop old ui

--- index.php
+++ index.php
@@ -1,65 +1,35 @@
 <?php 
-  
+
+  // Mock up Data  
   $tasks = [
-    ['id' => '1', 'title' => 'Wash the dishes'],
-    ['id' => '2', 'title' => 'Do laundry'],
-    ['id' => '3', 'title' => 'Make dinner']
+    [
+      'id' => '1',
+      'title' => 'Wash the dishes',
+      'status' => 'finished',
+      'create_date' => '27-06-2022',
+      'authors_id' => 1
+    ],
+    [
+      'id' => '2',
+      'title' => 'Run laps',
+      'status' => 'unfinished',
+      'create_date' => '27-06-2022',
+      'authors_id' => 1
+    ]
   ];
 
-  // Form was submitted
-  if(isset($_POST['submit'])) {
-    // Add new task to tasks array 
-    $new_task = [
-      'id' => count($tasks),
-      'title' => $_POST['task-title']
-    ];
-
-    array_push($tasks, $new_task);
-  }
+  $user = [
+    'id' => '1',
+    'username' => 'John Doe',
+    'email' => 'JohnDoe@todoapp.com',
+    'password' => 'johndoe123',
+    'join_date': '01-06-2022'
+  ];
 
 ?>
 
 <?php require_once './templates/header.php' ?>
 
-   <div class="mr-auto ml-auto mt-5 container">
-     <div class="card">
-      <div class="card-header">
-        <form action="" method="post" class="row">
-          <div class="form-group col-10 mb-0">
-            <label class="sr-only" for="inlineFormInput">Task Title</label>
-            <input type="text" class="form-control" id="inlineFormInput" placeholder="Task title" name="task-title" required>
-          </div>
-
-            <button type="submit" name="submit" class="btn btn-primary col-2">Add Task</button>
-        </form>
-      </div>
-
-      <div class="card-body">
-        <?php if(empty($tasks)) { ?>
-          <div class="card-text text-center">
-            Tasks you add will show up here
-          </div>
-        <?php } else { ?>
-          <?php foreach($tasks as $task) { ?>
-            <div class="card">
-              <div class="card-body row align-items-center" data-id="<?php echo $task['id'] ?>">
-                <h5 class="card-title mb-0 col-10"><?php echo $task['title'] ?></h5>
-
-                <div class="col-2 card-buttons">
-                  <a href="#" class="btn btn-danger">
-                    <i class="fa fa-trash"></i>
-                  </a>
-
-                  <a href="#" class="btn btn-warning">
-                    <i class="fa fa-pencil"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          <?php } ?>
-        <?php } ?>
-      </div>
-    </div>
-   </div>
+   
 
 <?php require_once './templates/footer.php' ?>

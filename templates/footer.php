12f2640883b1fbd3931409aab06a9aaf03245208
Omachonu Prosper <omachonucodes@gmail.com>
2022-07-20 16:15:48 +0100

    Use absolute path to load assets

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,6 @@
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-        <script src="../assets/js/jquery.js"></script>
-        <script src="../assets/js/bootstrap.bundle.js"></script>
-        <script type="text/javascript" src="../assets/js/app.js"></script>
+        <script src="/todo-app/assets/js/jquery.js"></script>
+        <script src="/todo-app/assets/js/bootstrap.bundle.js"></script>
+        <script type="text/javascript" src="/todo-app/assets/js/app.js"></script>
     </body>
 </html>
--- templates/header.php
+++ templates/header.php
@@ -6,10 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="/todo-app/assets/css/bootstrap.min.css">
         <!-- Font awesome CDN -->
-        <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="../assets/css/styles.css">
+        <link rel="stylesheet" type="text/css" href="/todo-app/assets/font-awesome/css/font-awesome.min.css">
+        <link rel="stylesheet" type="text/css" href="/todo-app/assets/css/styles.css">
 
 
       <title>Todo list app by Omachonu Prosper</title>

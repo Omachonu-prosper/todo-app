3d0bbdaed9fbfcd07f4ef8325ddc057c43cad528
Omachonu Prosper <omachonucodes@gmail.com>
2022-08-01 21:03:46 +0100

    Use correct linking to access files (Root path env variable)

--- templates/header.php
+++ templates/header.php
@@ -6,10 +6,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
         <!-- Bootstrap CSS -->
-        <link rel="stylesheet" type="text/css" href="/todo-app/assets/css/bootstrap.min.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo getenv('ROOT_PATH') . '/assets/css/bootstrap.min.css' ?>">
         <!-- Font awesome CDN -->
-        <link rel="stylesheet" type="text/css" href="/todo-app/assets/font-awesome/css/font-awesome.min.css">
-        <link rel="stylesheet" type="text/css" href="/todo-app/assets/css/styles.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo getenv('ROOT_PATH') . '/assets/font-awesome/css/font-awesome.min.css' ?>">
+        <link rel="stylesheet" type="text/css" href="<?php echo getenv('ROOT_PATH') . 'assets/css/styles.css' ?>">
 
 
       <title>Todo list app by Omachonu Prosper</title>
